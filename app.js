616cdd7eb2d31d6aa778ef2c8cc0591e07a5cb26
Ibrahim <106104213+ism-ibrahim@users.noreply.github.com>
2022-07-03 16:20:03 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+"use strict";
+$(document).ready(function () {
+
+});
+
+
+
+
+$('h2').click(function (e) { 
+    e.preventDefault();
+    $('h2').$(1234);
+});
+
+
+
+$('.btn').click(function (e) {
+    e.preventDefault();
+
+    let a = (a) => {
+        for (let i=1; i<=a; i++){
+            $('col-md-12').append(`<h1>${i}</h1>`);
+        }
+    }
+    a($('input').val())
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
