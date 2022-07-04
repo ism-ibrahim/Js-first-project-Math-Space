f16c3b7af5259fc1e2d5e64407f8cc9354a7db94
ism-ibrahim <iboss-97+second@mail.ru>
2022-07-04 00:11:42 +0000

    edit

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 "use strict";
 $(document).ready(function () {
 
-});
 
-
-
-
-$('h2').click(function (e) { 
-    e.preventDefault();
-    $('h2').$(1234);
-});
-
-
-
-$('.btn').click(function (e) {
-    e.preventDefault();
-
-    let a = (a) => {
-        for (let i=1; i<=a; i++){
-            $('col-md-12').append(`<h1>${i}</h1>`);
+    $('h2').click(function (e) { 
+        e.preventDefault();
+        $('h2').$(1234);
+    });
+    
+    
+    
+    $('.btn').click(function (e) {
+        e.preventDefault();
+    
+        let a = (a) => {
+            for (let i=1; i<=a; i++){
+                $('col-md-12').append(`<h1>${i}</h1>`);
+            }
         }
-    }
-    a($('input').val())
+        a($('input').val())
+    });
+
+console.log($('input').val);
+
+
 });
+
+
+
+
+
 
 
 
