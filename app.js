8bd55510483666b442d6abc359b569c88314df86
ism-ibrahim <iboss-97+second@mail.ru>
2022-07-08 18:28:22 +0000

    final

--- app.js
+++ app.js
@@ -2,31 +2,13 @@
 $(document).ready(function () {
 
 
-    // $('h2').click(function (e) { 
-    //     e.preventDefault();
-    //     $('h2').html('Congratulations');
-    // });
-
-    
-    $('.btn').click(function (e) {
-        e.preventDefault();
-    
-        let a = (a) => {
-            for (let i=1; i<=a; i++){
-                $('col-md-12').append(`<h1>${i}</h1>`);
-            }
-        }
-        a($('input').val())
-    });
-
-
     $('.btn').click(function (e) { 
         e.preventDefault();
         $('.btn').html('Congratulations');
     });
 
-});
 
+});
 
 
 
