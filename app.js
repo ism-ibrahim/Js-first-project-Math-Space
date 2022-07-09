1734a4e0177b5a93f3371d74d58c4867e5608284
ism-ibrahim <iboss-97+second@mail.ru>
2022-07-09 19:57:36 +0000

    final

--- app.js
+++ app.js
@@ -2,62 +2,20 @@
 $(document).ready(function () {
 
 
-    $('.btn').click(function (e) { 
+    $('.btn').click(function (e) {
         e.preventDefault();
-        $('.btn').html('Congratulations');
-
-        let p = (m) => {
-            for (let i=0; i<m; i++){
-                $(m).append(`<h1>${i}</h1>`);
+        let m = a => {
+            if (Number(a)) {
+                for (let i = 1; i <= a; i++) {
+                    $('.minibox').append(`<h1>${a}</h1>`);
+                }
+            } else{
+                $('.minibox').append(`<h1>${a}</h1>`);
             }
-            let a = b => (b) ? p(b) : a(0)
         }
-        a($('input').val(''))
+        m ($('input').val());
     });
-
 });
-
-
-
-    
-    // let p = m => {
-    //     for (let i = 0; i<m; i++) {console.log(m)}
-    // }
-    // let a = b => (b) ? p(b) : a(0)
-
-
-
-
-
-
-    // $('.btn').click(function (e) {
-    //     e.preventDefault();
-    
-    //     let a = (a) => {
-    //         for (let i=1; i<=a; i++){
-    //             $('col-md-12').append(`<h1>${i}</h1>`);
-    //         }
-    //     }
-    //     a($('input').val())
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
