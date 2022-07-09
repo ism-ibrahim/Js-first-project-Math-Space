8a50169cad744dd85c699cf95f43b86a9053a988
ism-ibrahim <iboss-97+second@mail.ru>
2022-07-09 19:07:47 +0000

    edit

--- app.js
+++ app.js
@@ -5,10 +5,44 @@ $(document).ready(function () {
     $('.btn').click(function (e) { 
         e.preventDefault();
         $('.btn').html('Congratulations');
+
+        let p = (m) => {
+            for (let i=0; i<m; i++){
+                $(m).append(`<h1>${i}</h1>`);
+            }
+            let a = b => (b) ? p(b) : a(0)
+        }
+        a($('input').val(''))
     });
 
-
 });
+
+
+
+    
+    // let p = m => {
+    //     for (let i = 0; i<m; i++) {console.log(m)}
+    // }
+    // let a = b => (b) ? p(b) : a(0)
+
+
+
+
+
+
+    // $('.btn').click(function (e) {
+    //     e.preventDefault();
+    
+    //     let a = (a) => {
+    //         for (let i=1; i<=a; i++){
+    //             $('col-md-12').append(`<h1>${i}</h1>`);
+    //         }
+    //     }
+    //     a($('input').val())
+    // });
+
+
+
 
 
 
