2f32d228eb0bb90f7dbbcb5e42419a7bbccb6499
ism-ibrahim <iboss-97+second@mail.ru>
2022-07-12 10:25:28 +0000

    edit

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 "use strict";
 $(document).ready(function () {
 
+    // $('.btn').click(function (e) {
+    //     e.preventDefault();
+    //     let f = a => {
+    //         if (Number(a)) {
+    //             for (let i = 1; i <= a; i++) {
+    //                 $('.minibox').append(`<h1>${a}</h1>`);
+    //             }
+    //         } else{
+    //             $('.minibox').append(`<h1>${a}</h1>`);
+    //         }
+    //     }
+    //     f ($('input').val());
+
     $('.btn').click(function (e) {
         e.preventDefault();
-        let f = a => {
-            if (Number(a)) {
-                for (let i = 1; i <= a; i++) {
-                    $('.minibox').append(`<h1>${a}</h1>`);
-                }
-            } else{
-                $('.minibox').append(`<h1>${a}</h1>`);
+        if ($.trim($('input').val()) == '') {
+            $('input').val();
+        } else {
+            let f = a => {
+                for (let i = 0; i < a; i++) { $('.minibox').append(`<h1>${a}</h1>`) };
             }
+            let z = e => Number(e) ? f(e) : $('.minibox').append(`<h1>${e}</h1>`);
+            z($.trim($('input').val()));
         }
-        f ($('input').val());
-
+        $('input').val('') 
     });
 
 });
+
+
+
+
+
 
 
 
