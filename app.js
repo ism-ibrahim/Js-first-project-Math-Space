e19d319a2cf62b3e4b7862285feaa7777cbfa454
ism-ibrahim <iboss-97+second@mail.ru>
2022-07-12 16:15:00 +0000

    edit

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 "use strict";
 $(document).ready(function () {
 
-    // $('.btn').click(function (e) {
-    //     e.preventDefault();
-    //     let f = a => {
-    //         if (Number(a)) {
-    //             for (let i = 1; i <= a; i++) {
-    //                 $('.minibox').append(`<h1>${a}</h1>`);
-    //             }
-    //         } else{
-    //             $('.minibox').append(`<h1>${a}</h1>`);
-    //         }
-    //     }
-    //     f ($('input').val());
 
     $('.btn').click(function (e) {
         e.preventDefault();
@@ -28,14 +16,5 @@ $(document).ready(function () {
         $('input').val('') 
     });
 
+
 });
-
-
-
-
-
-
-
-
-
-
